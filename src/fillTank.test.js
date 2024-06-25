1b108cd838df7108ecb4c6b7e1cab96fade92f23
SylwiaNG <iwon9@poczta.onet.pl>
2024-06-25 17:48:26 +0200

    Solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,75 @@
 'use strict';
 
+const { fillTank } = require('./fillTank');
+
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  it('should round the poured amount to the nearest tenth', () => {
+    const customer = {
+      money: 400,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 28,
+      },
+    };
 
-  it('should ', () => {
-
+    fillTank(customer, 50, 11.9);
+    expect(customer.vehicle.fuelRemains).toBeCloseTo(36, 1); // zmienione na 36
+    expect(customer.money).toBeCloseTo(400 - 8 * 50, 2); // zmienione na 8
   });
 
-  // write tests here
+  it('should round price to nearest hundredth', () => {
+    const customer = {
+      money: 1000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 20,
+      },
+    };
+
+    fillTank(customer, 33.333, 10);
+    expect(customer.vehicle.fuelRemains).toBeCloseTo(30, 1);
+    expect(customer.money).toBeCloseTo(1000 - 10 * 33.333, 2);
+  });
+
+  it('should fill tank to max if amount not given', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 50,
+        fuelRemains: 20,
+      },
+    };
+
+    fillTank(customer, 60);
+    expect(customer.vehicle.fuelRemains).toBeCloseTo(50, 1);
+    expect(customer.money).toBeCloseTo(3000 - 30 * 60, 2);
+  });
+
+  it('should pour amount that fits if too large', () => {
+    const customer = {
+      money: 2000,
+      vehicle: {
+        maxTankCapacity: 60,
+        fuelRemains: 50,
+      },
+    };
+
+    fillTank(customer, 50, 20);
+    expect(customer.vehicle.fuelRemains).toBeCloseTo(60, 1);
+    expect(customer.money).toBeCloseTo(2000 - 10 * 50, 2);
+  });
+
+  it('should not pour if amount is less than 2 liters', () => {
+    const customer = {
+      money: 100,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 10,
+      },
+    };
+
+    fillTank(customer, 50, 1);
+    expect(customer.vehicle.fuelRemains).toBe(10);
+    expect(customer.money).toBe(100);
+  });
 });
